1613f71976063e6e4c0b6599553d9094557e5f3d
Felipe Campinho <felipe.campinho@mirakulo.com>
2021-04-03 22:09:55 -0300

    add readme

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const app = express();
+
+// middleware para o express receber o JSON
+app.use(express.json());
+
+app.get("/courses", (request, response) => {
+  const query = request.query;
+  console.log(query)
+  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (request, response) => {
+  const body = request.body;
+  console.log(body)
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  const { id } = request.params;
+  console.log(id);
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 4"]);
+});
+
+//localhost:3031
+app.listen(3333, console.log("http://localhost:3333"));
