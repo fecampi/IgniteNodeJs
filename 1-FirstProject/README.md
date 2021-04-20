46fb28e7eb653b9b4032a981b5bf9c8b26d356d9
Felipe Campinho <felipe.campinho@mirakulo.com>
2021-04-19 22:28:28 -0300

    FirstProject finished

--- 1-FirstProject/README.md
+++ 1-FirstProject/README.md
@@ -1,26 +1,23 @@
-
 ##FinApi - Financeira
 
 ### REQUISITOS
 
+- [x]R01 - Deve ser possível criar uma conta;
+- [x]R02 - Deve ser possível buscar o extrato bancário do cliente;
+- [x]R03 - Deve ser possível realizar um depósito;
+- [X]R04 - Deve ser possível realizar um saque;
+- [X]R05 - Deve ser possível buscar o extrato bancário do cliente por data;
+- [X]R06 - Deve ser possível atualizar dados do cliente;
+- [x]R07 - Deve ser possível obter dados da conta do cliente;
+- [x]R08 - Deve ser possível deletar uma conta.
+- [x]R09 - Deve ser possível retornar o balance.
 
-### Requisitos
-
-- [] Deve ser possível criar uma conta;
-- [] Deve ser possível buscar o extrato bancário do cliente;
-- [] Deve ser possível realizar um depósito;
-- [] Deve ser possível realizar um saque;
-- [] Deve ser possível buscar o extrato bancário do cliente por data;
-- [] Deve ser possível atualizar dados do cliente;
-- [] Deve ser possível obter dados da conta do cliente;
-- [] Deve ser possível deletar uma conta.
 
 ### Regras de negócio
 
-- [] Não Deve ser possível cadastrar uma conta com CPF já existente;
-- [] Não Deve ser possível fazer depósito em uma conta não existente;
-- [] Não Deve ser possível buscar um extrato em uma conta não existente;
-- [] Não Deve ser possível fazer saque em uma conta não existente;
-- [] Não Deve ser possível excluir uma conta não existente;
-- [] Não Deve ser possível fazer saque quando o saldo foi insuficiente.
-
+- [x]RN01 - Não deve ser possível cadastrar uma conta com CPF já existente;
+- [x]RN02 - Não deve ser possível buscar extrato em uma conta inexistente;
+- [x]RN03 - Não deve ser possível fazer depósito em uma conta não existente;
+- [x]RN04 - Não deve ser possível fazer saque em uma conta inexistente;
+- [x]RN05 - Não deve ser possível fazer saque quando o saldo foi insuficiente.
+- [x]RN06 - Não deve ser possível excluir uma conta não existente
--- 1-FirstProject/src/index.js
+++ 1-FirstProject/src/index.js
@@ -1,9 +1,144 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+const customers = [];
 
-// middleware para o express receber o JSON
+// middleware Global
 app.use(express.json());
 
+// middleware - Regras de Negocio
+// RN02 - Não Deve ser possível fazer depósito em uma conta não existente;
+// RN03 - Não Deve ser possível buscar um extrato em uma conta não existente;
+// RN04 - Não Deve ser possível fazer saque em uma conta inexistente;
+// RN06 - Não deve ser possível excluir uma conta não existente
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+  request.customer = customer;
+  return next();
+}
+function getBalance(statement) {
+  //todos os valores em apenas um
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+  return balance;
+}
+
+// R01 - Deve ser possível criar uma conta;
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  // RN01 - Não Deve ser possível cadastrar uma conta com CPF já existente;
+  const customersAlreadyExist = customers.some(
+    (customers) => customers.cpf === cpf
+  );
+  if (customersAlreadyExist) {
+    return response.status(404).json({ error: "Customer already exists!" });
+  }
+  // RN01 - END
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+// R02 -Deve ser possível buscar o extrato bancário do cliente;
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.json(customer.statement);
+});
+
+// R03 - Deve ser possível realizar um depósito;
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+  customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
+
+// R04 - Deve ser possível realizar um saque;
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+  const balance = getBalance(customer.statement);
+  // RN05 - Não Deve ser possível fazer saque quando o saldo foi insuficiente.
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" });
+  }
+  // RN05 END
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+  customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
+
+//R05 - Deve ser possível buscar o extrato bancário do cliente por data;
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+  //Pegar qualquer horário do dia
+  const dateFormat = new Date(date + " 00:00");
+  //Somente o extrato bancário do dia
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement);
+});
+
+// R06 - Deve ser possível atualizar dados do cliente;
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+  customer.name = name;
+  return response.status(201).send();
+});
+
+// R07 - Deve ser possível obter dados da conta do cliente;
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.json(customer);
+});
+
+// R08 - Deve ser possível deletar uma conta.
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  customers.splice(customer, 1);
+  console.log(customers)
+  return response.status(200).json(customers);
+});
+
+// R09 - Deve ser possível retornar o balance.
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const balance = getBalance(customer.statement)
+  return response.json(balance);
+});
 
 app.listen(3333, console.log("http://localhost:3333"));
