a3e37ebb90430b35922e0ef0f1dedd8f665e7f66
Felipe Campinho <felipe.campinho@mirakulo.com>
2021-08-08 18:07:13 -0300

    added validations

--- Chapter_2.2-Api-Node/src/routes/categories.routes.ts
+++ Chapter_2.2-Api-Node/src/routes/categories.routes.ts
@@ -1,14 +1,29 @@
 import { Router } from "express";
 
+import CategoriesRepository from "../repositories/CategoriesRepository";
+
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories = [];
-
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
-  categories.push({ name, description });
-  console.log(categories);
+
+  // Validations - INIT
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already existis!" });
+  }
+  // Validations - END
+
+  categoriesRepository.create({ name, description });
+
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
